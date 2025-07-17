6866c022300a438eaeb3d15f1e25595b2045f2f3
unknown <81070048+leecheeyong@users.noreply.github.com>
2025-07-18 04:43:52 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ["firebase/firestore", "firebase/auth", "firebase/app"],
-    },
-  },
+  plugins: [vue(), tailwindcss()]
 });
